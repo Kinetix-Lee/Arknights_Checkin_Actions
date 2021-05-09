6e8d1bed8b0f969254076f74bbadaf6e68cfeb94
Kinetix <lee@x86.app>
2021-05-09 16:50:40 +0800

    Create network.js
    实现基础 GET/POST 功能

---
+++ utils/network.js
@@ -0,0 +1,25 @@
+const axios = require('axios')
+
+module.exports = {
+  get(url, params={}, headers={}) {
+    return new Promise((resolve, reject) => {
+      axios({
+        url,
+        method: 'get',
+        { headers },
+        { params }
+      })
+        .then(resolve).catch(reject)
+    }
+  },
+  post(url, data={}, headers={}) {
+    return new Promise((resolve, reject) => {
+      axios({
+        url,
+        method: 'post',
+        { headers },
+        { data }
+      })
+        .then(resolve).catch(reject)
+    }
+}
