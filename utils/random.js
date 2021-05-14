78c01574bad8a8dd21cdf654e27188b6df525ccc
Kinetix <x86app@126.com>
2021-05-14 22:17:52 +0800

    更新随机算法

--- utils/random.js
+++ utils/random.js
@@ -1,5 +1,5 @@
 require('../config')
-const { md5, sha256, HmacSHA1 } = require('CryptoJS')
+const CryptoJS = require('CryptoJS')
 module.exports = {
   randomDigits(length) {
     const max = 9
@@ -15,10 +15,11 @@ module.exports = {
   },
   randomString(length) {
     if (length > 64) return false // sha256 算法限制
-    return sha256(new Date().toString).slice(0, length)
+    return sha256(CryptoJS.lib.WordArray.random(128 / 8)).slice(0, length)
+    // TODO: 我也不知道这里为什么用sha256。word array的可行性会稍后验证。
   },
-  randomDeviceId = () => md5(this.randomString(12)),
+  randomDeviceId = () => CryptoJS.md5(this.randomString(12)),
   randomDeviceId2 = () => '86' + this.randomDigits(13),
-  randomDeviceId3 = () => md5(this.randomString(12)),
-  u8_sign = (data) => HmacSHA1(data, HMAC_KEY)
+  randomDeviceId3 = () => CryptoJS.md5(this.randomString(12)),
+  u8_sign = (data) => CryptoJS.HmacSHA1(data, HMAC_KEY)
 }
