95991d0761e75131883955e1bd7c7ee339fc0dab
Kinetix <x86app@126.com>
2021-05-15 16:08:16 +0800

    实现 network.js

--- README.md
+++ README.md
@@ -10,12 +10,12 @@
 - [x] class/Player.js | Player 类
 - [x] utils/account.js | 账号及游戏服务器登录
 - [x] utils/logger.js | 日志输出
-- [ ] utils/network.js | HTTP 请求
+- [x] utils/network.js | HTTP 请求
 - [x] utils/random.js | 字符串生成
 - [ ] utils/game.js | 游戏服务器相关操作
 - [ ] utils/serverchan.js | ServerChan 功能
 - [x] utils/checkInfo.js | 信息校对功能
-- [x] index.js | Node.js 入口文件
+- [ ] index.js | Node.js 入口文件
 - [ ] **国区基础功能**
 - [ ] **外区基础功能**
 - [ ] **多账号**
--- utils/network.js
+++ utils/network.js
@@ -2,37 +2,47 @@ const axios = require('axios')
 require('../config')
 
 module.exports = {
-  get(url, params={}, headers={}) {
+  // 基础 get/post
+  // 均自带 COMMON_HEADER，使用 Object.assign() 合并 headers
+  get(url, params={}, headersAddition={}) {
     return new Promise((resolve, reject) => {
       axios({
         url,
         method: 'get',
-        headers: Object.assign(COMMON_HEADER, headers),
+        headers: Object.assign(COMMON_HEADER, headersAddition),
         params
       }).then(resolve).catch(reject)
     })
   },
-  post(url, data={}, headers={}) {
+  post(url, data={}, headersAddition={}) {
     return new Promise((resolve, reject) => {
       axios({
         url,
         method: 'post',
-        headers: Object.assign(COMMON_HEADER, headers),
+        headers: Object.assign(COMMON_HEADER, headersAddition),
         data
       }).then(resolve).catch(reject)
     })
   },
 
   // 命名规律：请求方法+服务器
-  getConfig = (uri, params = {}, headers = {}) => this.get(USER.SERVER.CONF + uri, params, headers),
-  postAuth = (uri, data = {}, headers = {}) => this.post(USER.SERVER.AUTH + uri, data, headers),
-  postAccount = (uri, data = {}, headers = {}) => this.post(USER.SERVER.AUTH + uri, data, headers),
+
+  // 获取 配置信息
+  getConfig = (uri) => this.get(USER.SERVER.CONF + uri),
+
+  // 访问 账号认证服务器
+  postAuth = (uri, data = {}) => this.post(USER.SERVER.AUTH + uri, data),
+
+  // 访问 游戏服务器
   postGame(uri, data = {}, player) {
     let headers = {
       uid: player.uid,
       secret: player.secret,
       seqnum: player.seqnum
     }
-    return this.post(USER.SERVER.GAME + uri, data, Object.assign(COMMON_HEADER, headers))
-  }
+    return this.post(USER.SERVER.GAME + uri, data, headers)
+  },
+
+  // 访问 Yostar 账号服务器（外服专用）
+  postPassport = (uri, data = {}) => this.post(USER.SERVER.PASSPORT + uri, data)
 }
