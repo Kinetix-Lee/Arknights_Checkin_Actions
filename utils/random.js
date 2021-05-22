482f85660fa5693e251a75ab11e5f9d9da1f8607
Kinetix <x86app@126.com>
2021-05-22 18:12:50 +0800

    修复 random.js 语法错误
    这是对象属性，不是变量。
    在下失智博士（

--- utils/random.js
+++ utils/random.js
@@ -18,8 +18,8 @@ module.exports = {
     return sha256(CryptoJS.lib.WordArray.random(128 / 8)).slice(0, length)
     // TODO: 我也不知道这里为什么用sha256。word array的可行性会稍后验证。
   },
-  randomDeviceId = () => CryptoJS.md5(this.randomString(12)),
-  randomDeviceId2 = () => '86' + this.randomDigits(13),
-  randomDeviceId3 = () => CryptoJS.md5(this.randomString(12)),
-  u8_sign = (data) => CryptoJS.HmacSHA1(data, HMAC_KEY)
+  randomDeviceId: () => CryptoJS.md5(this.randomString(12)),
+  randomDeviceId2: () => '86' + this.randomDigits(13),
+  randomDeviceId3: () => CryptoJS.md5(this.randomString(12)),
+  u8_sign: (data) => CryptoJS.HmacSHA1(data, HMAC_KEY)
 }
